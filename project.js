9e25563fcbae921cdb3cf5ab08ca81f4873e5087
abguy6000 <112340114+abguy6000@users.noreply.github.com>
2023-03-16 19:22:03 +0100

    Add files via upload

--- project.js
+++ project.js
@@ -1,32 +1,75 @@
-let countEl = document.getElementById("count-el")
+let sum = 0;
+let blackjack = false;
+let alive = false;
+let cards = [];
+let message = "";
+let stats = {
 
-console.log(countEl)
-
-let count = 0
-
-function increment() {
-
-count += 1
-countEl.innerText = count
-
-}
-
-
-let saveEl = document.getElementById("save-el")
-
-function save() {
-
-    let seperator  = count
-
-    saveEl.innerText +=  count  + " - "
-    countEl.innerText = 0
-    count = 0
+name: "king",
+chips: 200,
 
 }
 
+let messageEl = document.getElementById("message-el");
+let cardsEl = document.getElementById("cards-el");
+let sumEl = document.getElementById("sum-el");
+let playerEl = document.getElementById("player-el")
 
 
+playerEl.innerText += stats.name + ": $" + stats.chips
 
 
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 11);
+  
+    if (randomNumber === 1 && sum <= 10) {
+      return 11;
+    } else if (randomNumber === 1 && sum > 10) {
+      return 1;
+    } else if (randomNumber > 10) {
+      return 10;
+    } else {
+      return randomNumber;
+    }
+  }
 
+function renderGame() {
+  messageEl.textContent = message;
+  sumEl.textContent = "Sum:" + sum;
+  cardsEl.textContent = "Card:";
 
+  for (let i = 0; i < cards.length; i += 1) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  if (sum === 21) {
+    message = "you've won the game MR.";
+    blackjack = true;
+  } else if (sum < 21) {
+    message = "want more cards bro?";
+    alive = true;
+  } else if (sum > 21) {
+    message = "Lost the game, idiot.";
+    alive = false;
+  }
+}
+
+function newCard() {
+  if (alive === true && blackjack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    console.log(cards);
+    renderGame();
+  }
+}
+
+function StartGame() {
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    sum = firstCard + secondCard;
+    cards = [firstCard, secondCard];
+    alive = true;
+    message = "";
+    renderGame();
+  }
